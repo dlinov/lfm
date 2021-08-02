484c9541dee762532e706c73d3c0346d971a3040
Dzmitryi Linou <dmitry.linov.4gplus@gmail.com>
2021-08-02 15:51:33 +0300

    Upgrade .NET to version 5

--- dotnet/run.sh
+++ dotnet/run.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 . ../*.env
-dotnet publish -c Release
-time ./bin/Release/netcoreapp3.1/publish/lfm $API_KEY $USER_NAME
+RID='osx-x64'
+dotnet publish -c Release -r $RID --self-contained true
+time ./bin/Release/net5.0/$RID/publish/lfm $API_KEY $USER_NAME
